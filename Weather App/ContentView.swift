05f054c803a565c542b4986240f5d72610bdd220
Moideen Nazaif VM <nazaifmoid@gmail.com>
2020-07-06 19:31:05 +0530

    Initial Commit

---
+++ Weather App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather App
+//
+//  Created by Moideen Nazaif VM on 06/07/20.
+//  Copyright © 2020 Moideen Nazaif VM. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
